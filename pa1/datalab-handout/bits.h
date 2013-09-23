ebb898fb7a26e4f4dbf88ad12d80b6d6d6386beb
TWaltze <tylerwaltze@gmail.com>
2013-09-23 00:47:02 -0400

    Initial commit + bitAnd and tmin functions

---
+++ pa1/datalab-handout/bits.h
@@ -0,0 +1,24 @@
+// Rating: 1
+int bitAnd(int, int);
+int test_bitAnd(int, int);
+int tmin();
+int test_tmin();
+// Rating: 2
+int getByte(int, int);
+int test_getByte(int, int);
+int fitsBits(int, int);
+int test_fitsBits(int, int);
+int divpwr2(int, int);
+int test_divpwr2(int, int);
+int negate(int);
+int test_negate(int);
+// Rating: 3
+int logicalShift(int, int);
+int test_logicalShift(int, int);
+int isPositive(int);
+int test_isPositive(int);
+int isLessOrEqual(int, int);
+int test_isLessOrEqual(int, int);
+// Rating: 4
+int bang(int);
+int test_bang(int);
