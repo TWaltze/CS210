592f81a9a770de5c94a6a5eb86b51177a81c4277
TWaltze <tylerwaltze@gmail.com>
2013-09-24 00:13:39 -0400

    This might just be reverting the last change.

--- pa1/datalab-handout/bits.c
+++ pa1/datalab-handout/bits.c
@@ -313,7 +313,7 @@ int isLessOrEqual(int x, int y) {
   int lessThan;
 
   // x - y (add y's complement to x)
-  difference = (~x + 1) + (~y + 1);
+  difference = x + (~y + 1);
   printf("%d - %d = %d\n", x, y, difference);
 
   // x == y
@@ -326,7 +326,7 @@ int isLessOrEqual(int x, int y) {
   // If so, the difference will be positive,
   // meaning the far left will be 0. Move that
   // left most to right most.
-  lessThan = (((~x + 1) + (~y + 1)) & (1 << 31)) >> 31;
+  lessThan = (difference & (1 << 31)) >> 31;
   printf("%d < %d? %d\n\n", y, x, lessThan);
 
   //
